76ffa9c4c15ebebaee0f7317eeeb8ddc758cca94
George Ananda <georgeananda@gmail.com>
2022-01-14 16:33:50 +0700

    Initial Commit

---
+++ BoxBoxPersistence/BoxBoxPersistence.h
@@ -0,0 +1,18 @@
+//
+//  BoxBoxPersistence.h
+//  BoxBoxPersistence
+//
+//  Created by George Ananda on 14/01/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BoxBoxPersistence.
+FOUNDATION_EXPORT double BoxBoxPersistenceVersionNumber;
+
+//! Project version string for BoxBoxPersistence.
+FOUNDATION_EXPORT const unsigned char BoxBoxPersistenceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoxBoxPersistence/PublicHeader.h>
+
+
